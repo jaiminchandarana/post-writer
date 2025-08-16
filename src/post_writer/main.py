5702bf1971bb7ef947f6d888ce44b701f479b985
jaimin <jdchandarana3@gmail.com>
2025-08-16 13:10:44 +0530

    Uploading whole project

---
+++ src/post_writer/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import sys
+import warnings
+from post_writer.crew import PostWriter
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+topic = input("Enter a plateform to write post for : ")
+description = input("Provide some information of your project : ")
+code = input("paste your main file here(optional) : ")
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': f'{topic}',
+        'description' : f'{description}',
+        'code' : f'{code}'
+    }
+    
+    try:
+        PostWriter().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        'topic': f'{topic}',
+        'description' : f'{description}',
+        'code' : f'{code}'
+    }
+    try:
+        PostWriter().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        PostWriter().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        'topic': f'{topic}',
+        'description' : f'{description}',
+        'code' : f'{code}'
+    }
+    
+    try:
+        PostWriter().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
